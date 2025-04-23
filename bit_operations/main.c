1b9dcbf552d08697c5b907df9345b84a9880fed9
Kaldheim531 <kaldheim531@gmail.com>
2025-04-23 23:07:19 +0700

    homework

---
+++ bit_operations/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void print_bytes(int a) {
+  for (char i = (sizeof(a) * 8) - 1; i >= 0; i--) {
+    printf("%x", (a >> i) & 1);
+    if (i % 8 == 0) {
+      printf(" ");
+    }
+  }
+  puts("");
+}
+
+void counting_units(int a) {
+  char counter = 0;
+  for (char i = (sizeof(a) * 8) - 1; i >= 0; i--) {
+    if ((a >> i) & 1) {
+      counter += 1;
+    }
+  }
+  printf("Кол-во единиц = %d\n", counter);
+}
+
+void swap_third_byte(int a, int c) {
+  a = (a & 0xFF00FFFF) | (((c & 0xFF) << 16));
+  print_bytes(a);
+}
+
+int main() {
+  int number;
+  printf("input positive number: ");
+  scanf("%d", &number); 
+  print_bytes(number);
+  counting_units(number);
+  puts("");
+
+  int thirdByte;
+  printf("input third byte: ");
+  scanf("%d", &thirdByte);
+  swap_third_byte(number, thirdByte);
+  puts("");
+
+  int negative;
+  printf("input negative number: ");
+  scanf("%d", &negative); 
+  print_bytes(negative);
+
+  return 0;
+}
